1d8f5b47f833659bdc433b73dfeba10b88e36b03
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-03-09 17:20:32 +0300

    done

--- Header.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Header() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Header</Text>
-    </View>
-  );
-}
